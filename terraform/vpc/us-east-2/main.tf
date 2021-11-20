d8564ceac4ecdbdd56556311422999f02e4cf717
Josh Feierman <josh@sqljosh.com>
2021-11-20 10:54:04 -0500

    Added VPC deployment

---
+++ terraform/vpc/us-east-2/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~>3"
+      source  = "hashicorp/aws"
+    }
+  }
+  required_version = "~>1"
+}
+
+provider aws {
+  region = "us-east-2"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "k8slab"
+  cidr = "10.0.1.0/24"
+
+  azs             = ["us-east-2a"]
+    public_subnets  = ["10.0.1.0/27"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    "repo" = "yardbirdsax/infra-k8slab"
+  }
+}

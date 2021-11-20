6dfbd710ed4656029389b45c5c2e0fbd725a7519
Josh Feierman <josh@sqljosh.com>
2021-11-20 10:57:58 -0500

    Terraform fmt

--- terraform/vpc/us-east-2/main.tf
+++ terraform/vpc/us-east-2/main.tf
@@ -8,19 +8,19 @@ terraform {
   required_version = "~>1"
 }
 
-provider aws {
+provider "aws" {
   region = "us-east-2"
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "3.11.0"
 
   name = "k8slab"
   cidr = "10.0.1.0/24"
 
-  azs             = ["us-east-2a"]
-    public_subnets  = ["10.0.1.0/27"]
+  azs            = ["us-east-2a"]
+  public_subnets = ["10.0.1.0/27"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
